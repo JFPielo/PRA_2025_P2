0dce6e9de96a298a59aec03d8f4011ac650a9238
Josué Félix Piedrasanta Loarca <jfpielo1@etsinf.upv.es>
2025-09-30 14:23:01 -0600

    Tipos de quicksort con el tiempo y arreglo en BusquedaBinaria inversa

--- DyV.h
+++ DyV.h
@@ -1,5 +1,5 @@
 #include <vector>
-
+#include <random>
 int BusquedaBinaria(int x, std::vector<int>& v, int ini, int fin){
 	int medio = (ini + fin) / 2;
 	if (ini > fin){
@@ -29,10 +29,10 @@ int BusquedaBinaria_INV(int x, std::vector<int>& v, int ini, int fin){
 	}
 	else {
 		if (v[medio] < x){
-			return BusquedaBinaria(x,v,ini,medio-1);
+			return BusquedaBinaria_INV(x,v,ini,medio-1);
 		}
 		else{
-			return BusquedaBinaria(x,v,ini+1,fin);
+			return BusquedaBinaria_INV(x,v,ini+1,fin);
 		}
 	}
 }
@@ -57,6 +57,37 @@ void QuickSort(std::vector<int>& v, int ini, int fin){
 		int pivot = Partition(v,ini,fin);
 		QuickSort(v,ini,pivot-1);
 		QuickSort(v,pivot+1,fin);
+	}
+}
+
+void QuickSort_Primero(std::vector<int>& v, int ini, int fin){
+	if (ini < fin){
+		std::swap(v[ini],v[fin]);
+		int pivot = Partition(v,ini,fin);
+		QuickSort_Primero(v,ini,pivot-1);
+		QuickSort_Primero(v,pivot+1,fin);
+	}
+}
+
+
+void QuickSort_Centro(std::vector<int>& v, int ini, int fin){
+	if (ini < fin){
+		int centro = ini + (fin - ini)/2;
+		std::swap(v[centro],v[fin]);
+		int pivot = Partition(v,ini,fin);
+		QuickSort_Centro(v,ini,pivot-1);
+		QuickSort_Centro(v,pivot+1,fin);
+	}
+}
+
+
+void QuickSort_Random(std::vector<int>& v, int ini, int fin){
+	if (ini < fin){
+		int random = ini + rand() % (fin - ini + 1);
+		std::swap(v[random],v[fin]);
+		int pivot = Partition(v,ini,fin);
+		QuickSort_Random(v,ini,pivot-1);
+		QuickSort_Random(v,pivot+1,fin);
 	}
 }
 
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,41 +1,92 @@
 #include <vector>
 #include "DyV.h"
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 int main(){
-	vector<int> a{1,2,3,4,5,6,7,8,9,10};
+	srand(time(NULL));
+	vector<int> ORD{1,2,3,4,5,6,7,8,9,10};
 	vector<float> a1{1,2,3,4,5,6,7,8,9,10};
 	vector<int> b{10,9,1,2,8,7,3,4,5,6};
 	vector<char> a2{1,2,3,4,5,6,7,8,9,10};
 	vector<int> c{1,3,5,7,9,2,4,6,8,10};
-	vector<int> c1{10,9,8,7,6,5,4,3,2,1};
+	vector<int> ORD2{10,9,8,7,6,5,4,3,2,1};
 
 
         cout << "Ingrese el número que desea buscar: ";
 	int x;
 	cin >> x;
-	int res = BusquedaBinaria(x,a,0,a.size());
+	int res = BusquedaBinaria(x,ORD,0,ORD.size());
 	if (res != -1){
 		cout << "Se encuentra en la posición: " << res << " del vector en Busqueda Binaria Normal"	<< endl;
 	} else {
 		cout << "Número no encontrado" << endl;
 	}
 		
-	int res2 = BusquedaBinaria_INV(x,c1,0,c1.size());
+	int res2 = BusquedaBinaria_INV(x,ORD2,0,ORD2.size());
 	if (res2 != -1){
 		cout << "Se encuentra en la posición: " << res2 << " del vector en Busqueda Binaria Inversa"	<< endl;
 	} else {
 		cout << "Número no encontrado" << endl;
 	}
+	vector<int> c_prueba1 = c;
+	vector<int> c_prueba2  = c;
+	vector<int> c_prueba3 = c;
+	vector<int> c_prueba4 = c;
+	vector<int> c_prueba5 = c;
+	auto start_1 = chrono::system_clock::now();
+        QuickSort(c_prueba1,0,c_prueba1.size()-1);
+	auto end_1 = chrono::system_clock::now();
+	chrono::duration<float, std::milli> duration_1 = end_1 -start_1;
 
-        QuickSort(c,0,c.size()-1);
+	cout << "Elementos ordenados normal en un tiempo de: " << duration_1.count() << " s" << endl;
 	cout << "Elementos: ";
-	for (int valor : c){
+	for (int valor : c_prueba1){
 		cout << valor << " ";
 		}
 	cout << endl;
+
+	auto start_2 = chrono::system_clock::now();
+        QuickSort_Primero(c_prueba2,0,c_prueba2.size()-1);
+	auto end_2 = chrono::system_clock::now();
+	chrono::duration<float, std::milli> duration_2 = end_2 -start_2;
+
+	cout << "Elementos ordenados con primero un tiempo de: " << duration_2.count() << " s" << endl;
+	cout << "Elementos: ";
+	for (int valor : c_prueba2){
+		cout << valor << " ";
+		}
+	cout << endl;
+
+	auto start_3 = chrono::system_clock::now();
+        QuickSort_Centro(c_prueba3,0,c_prueba3.size()-1);
+	auto end_3 = chrono::system_clock::now();
+	chrono::duration<float, std::milli> duration_3 = end_3 -start_3;
+
+	cout << "Elementos ordenados con centro un tiempo de: " << duration_3.count() << " s" << endl;
+	cout << "Elementos: ";
+	for (int valor : c_prueba3){
+		cout << valor << " ";
+		}
+	cout << endl;
+
+
+	auto start_4 = chrono::system_clock::now();
+        QuickSort_Random(c_prueba4,0,c_prueba4.size()-1);
+	auto end_4 = chrono::system_clock::now();
+	chrono::duration<float, std::milli> duration_4 = end_4 -start_4;
+
+	cout << "Elementos ordenados con random un tiempo de: " << duration_4.count() << " s" << endl;
+	cout << "Elementos: ";
+	for (int valor : c_prueba4){
+		cout << valor << " ";
+		}
+	cout << endl;
+
+
 	return 0;	
+	
 }
 
 
